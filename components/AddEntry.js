721bc2ac33c8e25b1c2832bb551a3d66a7f59390
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2017-12-04 22:09:38 -0500

    Changes for AddEntry-DateHeader

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,7 @@ import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
 
 export default class AddEntry extends Component {
 
@@ -49,6 +50,7 @@ export default class AddEntry extends Component {
 
         return (
             <View>
+                <DateHeader date={ (new Date()).toLocaleDateString()}/>
                 {
                     Object.keys(metaInfo).map((key) => {
                         const { getIcon, type, ...rest } = metaInfo[key]
