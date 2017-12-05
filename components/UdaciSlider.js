440cebe3bfa29a984b297c0981fd1eccd0ab6f75
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2017-12-05 16:27:07 -0500

    Changes for AddEntry-UdaciSlider

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Slider } from 'react-native'
 
-export default function UdaciSlider() {
+export default function UdaciSlider({max, unit, step, value, onChange}) {
     return (
         <View>
-            <Text>UdaciSlider</Text>
+            <Slider
+                value={value}
+                step={step}
+                maximumValue={max}
+                minimumValue={0}
+                onValueChange={onChange}
+            />
+
+            <Text>{value}</Text>
+            <Text>{unit}</Text>
         </View>
     )
 }
