e38127e0702910bd3357edb3cb13450883eb2b1d
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2017-12-10 00:25:08 -0500

    Changes for reducers

---
+++ reducers/index.js
@@ -0,0 +1,22 @@
+import {ADD_ENTRY, RECEIVE_ENTRIES} from "../actions"
+
+function entries (state = {}, action) {
+    switch (action.type) {
+        case RECEIVE_ENTRIES:
+            return {
+                ...state,
+                ...action.entries
+            }
+
+        case RECEIVE_ENTRIES:
+            return {
+                ...state,
+                ...action.entry
+            }
+
+        default:
+            return state
+    }
+}
+
+export default entries
