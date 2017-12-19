9a5b74413e5d41e8b81cf247ac626da6c13a799d
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2017-12-19 09:38:01 -0500

    Changes for History

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
+import History from './components/History'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
@@ -10,7 +10,7 @@ export default class App extends React.Component {
     return (
         <Provider store={createStore(reducer)}>
           <View style={{flex: 1}}>
-            <AddEntry/>
+            <History/>
           </View>
         </Provider>
     );
---
+++ components/History.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import { View, Text } from 'react-native'
+import {connect} from 'react-redux'
+import {addEntry, receiveEntries} from '../actions'
+import {timeToString, getDailyReminderValue} from '../utils/helpers'
+import {fetchCalendarResults} from '../utils/api'
+
+class History extends Component {
+
+    componentDidMount() {
+        const {dispatch} = this.props
+
+        fetchCalendarResults()
+            .then((entries) => dispatch(receiveEntries(entries)))
+            .then(({entries}) => {
+                if(!entries[timeToString()]) {
+                    dispatch(addEntry({
+                        [timeToString()]: getDailyReminderValue()
+                    }))
+                }
+            })
+    }
+
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Text>{JSON.stringify(this.props)}</Text>
+            </View>
+        );
+    }
+}
+
+function mapStateToProps(entries) {
+    return { entries }
+}
+
+export default connect(mapStateToProps)(History)
