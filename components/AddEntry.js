1a213c16cd66c28ed05165e50ad47bdb4e48aa2d
Manoj Talagadadeevi <manoj.talagadadeevi@ey.com>
2017-12-20 16:21:13 -0500

    Changes for /AddEntry - Navigate back to home after Submit / Reset

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -14,6 +14,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { purple, white } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -24,6 +25,7 @@ function SubmitBtn ({ onPress }) {
     </TouchableOpacity>
   )
 }
+
 class AddEntry extends Component {
   state = {
     run: 0,
@@ -69,12 +71,13 @@ class AddEntry extends Component {
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
 
-    // Navigate to home
+    this.toHome()
 
     submitEntry({ key, entry })
 
     // Clear local notification
   }
+
   reset = () => {
     const key = timeToString()
 
@@ -82,10 +85,16 @@ class AddEntry extends Component {
       [key]: getDailyReminderValue()
     }))
 
-    // Route to Home
+    this.toHome()
 
     removeEntry(key)
   }
+
+
+  toHome () {
+      this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}))
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
 
